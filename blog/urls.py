045dd8241a87c24d9769699426a13846933debce
N2-Met <yuyushiki11@gmail.com>
2023-06-04 07:33:29 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,7 @@ from . import views
 # post_list という名前の ビュー をルートURLに割り当てる
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    # もし post/何かしらの数字/ の形のURLがリクエストされたら、post_detail ビューを呼び出し、
+    # そのビューにパラメータとして pk を渡す" というルールを Django に教えています。そして、このルールの名前は post_detail です。
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
