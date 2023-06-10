e45f790b784a8b28a706d5527e1f5996d3f00882
N2-Met <yuyushiki11@gmail.com>
2023-06-10 10:45:48 +0900

    Added views to create/edit blog post inside site.

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     # もし post/何かしらの数字/ の形のURLがリクエストされたら、post_detail ビューを呼び出し、
     # そのビューにパラメータとして pk を渡す" というルールを Django に教えています。そして、このルールの名前は post_detail です。
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
